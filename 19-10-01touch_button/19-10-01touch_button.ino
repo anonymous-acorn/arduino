bccb1f406ca9be552ebac36bfc66763c6f6b3568
anonymous-acorn <72636265+anonymous-acorn@users.noreply.github.com>
2020-10-17 11:22:40 +0900

    Add files via upload

---
+++ 19-10-01touch_button/19-10-01touch_button.ino
@@ -0,0 +1,28 @@
+void setup() {
+  pinMode(7,INPUT);
+  pinMode(8,INPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  Serial.begin(9600);
+ 
+  
+}
+
+void loop() {
+  Serial.println(digitalRead(7));
+  if(digitalRead(7) == HIGH)
+  {
+    digitalWrite(9, HIGH);
+  }
+  else if(digitalRead(8) == HIGH)
+  {
+    digitalWrite(10,HIGH);
+  }
+  else
+  {
+    digitalWrite(9,LOW);
+    digitalWrite(10,LOW);
+  }
+  
+}
